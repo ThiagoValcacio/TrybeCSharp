7412e6aeba3314f890ec96d6c238a1e6afdb9422
ThiagoValcácio <thiagoassis963741@gmail.com>
2025-05-26 23:32:37 -0300

    Estrutura new web Api + Pastas

---
+++ ProjetoReservasSalas/Program.cs
@@ -0,0 +1,24 @@
+using ProjetoReservasSalas.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Injeção de dependência
+builder.Services.AddSingleton<SalaService>();
+builder.Services.AddSingleton<ReservaService>();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
